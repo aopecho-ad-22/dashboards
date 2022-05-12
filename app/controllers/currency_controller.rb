0f95c8a7da186c78bee58f4e55b091d907a5b3a8
aopecho <aopecho@uchicago.edu>
2022-05-12 00:02:06 +0000

    finishing up currency conversion

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -21,4 +21,18 @@ class CurrencyController < ActionController::Base
 
     render ({ :template => "currency_templates/second_currency.html.erb" })
   end
+
+  def convert
+
+    @first_currency = params.fetch("first_currency")
+    @second_currency = params.fetch("second_currency")
+
+    @api_url = "https://api.exchangerate.host/convert?from=" + @first_currency + "&to=" + @second_currency
+
+    @raw_currency_data = open(@api_url).read
+    @parsed_currency_data = JSON.parse(@raw_currency_data)
+    @converted_amount = @parsed_currency_data.fetch("result")
+
+    render ({ :template => "currency_templates/conversion.html.erb" })
+  end
 end
